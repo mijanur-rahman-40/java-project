370941cc5e9061783e293c15516ea5d627554349
hasansust32 <smmahamudulhasan32@student.sust.edu>
2021-01-28 21:07:20 +0600

    main file changed

--- SkyForce/src/SkyForce/Main.java
+++ SkyForce/src/SkyForce/Main.java
@@ -2,7 +2,7 @@ package SkyForce;
 
 public class Main {
     public static void main(String[] args) {
-        GameSetup game = new GameSetup("SkyForce",500,600);
+       
         // game.start();
     }
 }
